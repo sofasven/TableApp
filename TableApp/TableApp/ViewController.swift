348c55c4f40c875e95d60cc7ed98aeda3805d609
Sofa <sofasven00@gmail.com>
2023-09-13 20:15:14 +0300

    Project created

---
+++ TableApp/TableApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableApp
+//
+//  Created by Sofa on 13.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
