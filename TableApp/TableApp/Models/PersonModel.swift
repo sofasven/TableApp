a154fedbe201c2af7f66f68f05aaacf7f162a610
Sofa <sofasven00@gmail.com>
2023-09-14 00:31:33 +0300

    Part of task

---
+++ TableApp/TableApp/Models/PersonModel.swift
@@ -0,0 +1,15 @@
+//
+//  PersonModel.swift
+//  TableApp
+//
+//  Created by Sofa on 13.09.23.
+//
+
+import UIKit
+
+struct Person {
+    var name: String
+    var surname: String
+    var email: String
+    var numberPhone: String
+}
